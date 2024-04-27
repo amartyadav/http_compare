45d0bdd8d4a36ea2b5b21589aa441b03880a2f28
amartya <arvyagami@proton.me>
2024-04-27 20:13:45 +0530

    Refactor RequestObject struct to use Option<String> for body and Vec<HeaderObject> for headers

--- src-tauri/src/structs/request_object.rs
+++ src-tauri/src/structs/request_object.rs
@@ -1,6 +1,10 @@
+use serde::{Deserialize, Serialize};
+
+use super::HeaderObject;
+#[derive(Serialize, Deserialize, Debug)]
 pub struct RequestObject {
-    pub body: String,
-    pub headers: String,
+    pub body: Option<String>,
+    pub headers: Vec<HeaderObject>,
     pub method: String,
     pub url: String
 }
