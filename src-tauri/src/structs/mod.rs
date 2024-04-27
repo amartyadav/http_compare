8d3f90c25fed1d53119cd886da32b214b808773d
amartya <arvyagami@proton.me>
2024-04-27 20:14:06 +0530

    Add HeaderObject struct and update imports in mod.rs

--- src-tauri/src/structs/mod.rs
+++ src-tauri/src/structs/mod.rs
@@ -1,7 +1,9 @@
 mod main_object;
 mod response_object;
 mod request_object;
+mod header_object;
 
-pub use main_object::MainObject;
+pub use main_object::{MainObject, MainObjects};
 pub use response_object::ResponseObject;
 pub use request_object::RequestObject;
+pub use header_object::HeaderObject;
