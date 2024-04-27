1dac7bbca51db8cf27da946077e2dbbc73d15157
amartya <arvyagami@proton.me>
2024-04-27 20:13:52 +0530

    Refactor ResponseObject struct to use Option<String> for body and Vec<HeaderObject> for headers

--- src-tauri/src/structs/response_object.rs
+++ src-tauri/src/structs/response_object.rs
@@ -1,6 +1,11 @@
+use serde::{Deserialize, Serialize};
+
+use super::HeaderObject;
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct ResponseObject {
-    pub body: String,
-    pub headers: String,
+    pub body: Option<String>,
+    pub headers: Vec<HeaderObject>,
     pub status_code: u16,
     pub status_text: String
 }
