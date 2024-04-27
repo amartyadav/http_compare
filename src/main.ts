57b7823cd200e292f268a28156b8bae8a613bbd6
amartya <arvyagami@proton.me>
2024-04-27 20:14:49 +0530

    Update file opener buttons to select multiple files and handle errors

--- src/main.ts
+++ src/main.ts
@@ -15,20 +15,40 @@ async function greet() {
 
 async function openInputFileSelector() {
   const selected = await open({
-    multiple: false,
+    multiple: true,
     filters: [{
       name: "JSON",
       extensions: ["json"]
     }]
   });
+  // add an error message if no file was selected
+  if (selected === null || selected.length === 0) {
+    console.error("No file selected");
+    return;
+  }
+
+  // add an error message if more than 2 files were selected
+  if (selected.length > 2) {
+    console.error("Please select only 2 files");
+    return;
+  }
+
   console.group("Selected files");
   console.log(selected);
+  console.groupEnd();
+
+  let result = await invoke("read_and_parse_json", {
+    jsonFilePaths: selected
+  });
+
+  console.group("Result");
+  console.log(result);
   console.groupEnd();
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#select-file1")?.addEventListener("click", openInputFileSelector);
-  document.querySelector("#select-file2")?.addEventListener("click", openInputFileSelector);
+  document.querySelector("#select-files")?.addEventListener("click", openInputFileSelector);
+  // document.querySelector("#select-file2")?.addEventListener("click", openInputFileSelector);
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
   document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
