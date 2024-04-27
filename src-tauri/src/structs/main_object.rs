5dbbcbe7db23e403cfd5dd80326a0b994d1d52f8
amartya <arvyagami@proton.me>
2024-04-27 20:13:36 +0530

    Refactor MainObject struct to use RequestObject and ResponseObject

--- src-tauri/src/structs/main_object.rs
+++ src-tauri/src/structs/main_object.rs
@@ -1,5 +1,11 @@
+use serde::{Deserialize, Serialize};
+use crate::structs::{RequestObject, ResponseObject};
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct MainObject {
     pub http_version: String,
-    pub request: String,
-    pub response: String
+    pub request: RequestObject,
+    pub response: ResponseObject
 }
+
+pub type MainObjects = Vec<MainObject>;
